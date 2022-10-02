e85083261b6a19dc2fb610fe4c811f3170a804f0
IN THE DOOR 2 <kristoffdixon19@gmail.com>
2022-10-01 20:14:43 -0400

    Created a dictionary log for both the main hero Hercules and the main villain Alucard. I also created a attack function that will return the new target health. Also added a random attack function that will require a list of attacks that the Monster has.

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
+import random
+
 print("Welcome to the Python RPG!")
+
+HerculesDict = {
+  "Name" : "Hercules",
+  "Health": 200,
+  "Attack Power": 20,
+  "Mighty Slash": 1.5,
+  "Shield Bash": 1.5,
+  "Holy Right": 5.0
+
+}
+
+AlucardDict = {
+    "Name" : "Alucard",
+    "Health" : 400,
+    "Attack Power" : 50
+}
+
+def Attack(target_health,attack_power, skill):
+    attack_damage = attack_power * skill
+    target_health -= attack_damage
+    return target_health
+
+def random_enemy_attack(attack_list):
+    attack = random.choice(attack_list)
+    return attack
+
+
+
